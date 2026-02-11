f431b8f45945febe00066147977665d820c60ebd
Triolingo220 <wg27schlegele@365.bk-lk.de>
2026-02-11 14:17:13 +0100

    Update word.js

--- word.js
+++ word.js
@@ -1,16 +1,1 @@
-const words = {
-  englisch: [
-    ["the","der/die/das"],["be","sein"],["to","zu"],["of","von"],["and","und"],
-    ["a","ein/eine"],["in","in"],["that","dass"],["have","haben"],["I","ich"],
-    ["it","es"],["for","für"],["not","nicht"],["on","auf"],["with","mit"],
-    ["he","er"],["as","wie"],["you","du/Sie"],["do","tun"],["at","bei"]
-  ],
-  russisch: [
-    ["я","ich"],["ты","du"],["он","er"],["она","sie"],["мы","wir"],
-    ["вы","ihr/Sie"],["они","sie"],["это","das ist"],["что","was"],["не","nicht"]
-  ],
-  kurdisch: [
-    ["ez","ich"],["tu","du"],["ew","er/sie/es"],["em","wir"],["hûn","ihr"],
-    ["ew","sie (Pl)"],["ev","dies"],["ji","von"],["li","in/an"],["bi","mit"]
-  ]
-};
+
